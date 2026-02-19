206442c4e0bb6649dadd6cf9a7bed2b838ee589d
KANIGAVARSHA K <223409386+kaniga-59@users.noreply.github.com>
2026-02-19 15:03:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (32.64%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,28 @@
+bool isValid(char* s) {
+    char stack[10000];
+    int top=-1;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c=s[i];
+
+        if (c == '(' || c == '{' || c == '[') {
+            stack[++top]=c;
+        } 
+        else if (c == ')' || c == '}' || c == ']') {
+            if (top==-1)  
+                return false;
+
+            char open=stack[top--];
+
+            if ((c == ')' && open != '(') ||
+                (c == '}' && open != '{') ||
+                (c == ']' && open != '[')) {
+                return false;
+            }
+        }
+    }
+
+    return top==-1;
+}
+
+    

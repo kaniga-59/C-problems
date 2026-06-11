0f90390cb7ffebdf35857ff5e1e312ec7e2db3b0
KANIGAVARSHA K <223409386+kaniga-59@users.noreply.github.com>
2026-06-11 21:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (66.91%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.c
@@ -0,0 +1,10 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int i;
+    for(i=0;i<numsSize;i++)
+    {
+     if(target<=nums[i])
+     
+        return i;
+    }
+     return numsSize;
+}
